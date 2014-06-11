781f0189eb6301e1837c7928f03282d639b09f5a
daicham <daicham@gmail.com>
2014-06-11 15:41:02 +0900

    Add a controller method which returns json
    Also add jackson-databind in build.gradle

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,18 +1,29 @@
 package hello;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
 @EnableAutoConfiguration
 public class SampleController {
 
+    // Return just string
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
+    public String home() {
         return "Hello World!";
+    }
+
+    // Return Object as JSON (using jackson-databind)
+    private final AtomicLong counter = new AtomicLong();
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),String.format("Hello, %s", name));
     }
 
     public static void main(String... args) {
